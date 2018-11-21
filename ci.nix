8d275694d905fb20d5e9031bf3600a2ada2155bb
Richard Marko <srk@48.io>
2018-11-21 16:46:59 +0100

    init

---
+++ ci.nix
@@ -0,0 +1,4 @@
+{ pkgs ? import ./nixpkgs.nix }:
+{
+  hello = pkgs.hello;
+}
---
+++ nixpkgs.nix
@@ -0,0 +1,9 @@
+import (
+  fetchTarball {
+    url = https://github.com/nixos/nixpkgs-channels/archive/a06177e65ac17ca0f043f81f6eeb5223290cbaca.tar.gz;
+    sha256 = "1fnr0sm205lj2s1pkzskva5riq8p5fiv58995p1a5gmjrfq0kw2s";
+  }
+) {
+  config = {};
+  overlays = [ ];
+}
